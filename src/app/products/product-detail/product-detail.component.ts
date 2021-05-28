4527a0b29ecb5e66545f3d5e7c5119d4aa3e0c11
ldex <laurent.duveau+git@gmail.com>
2021-05-28 15:45:59 -0400

    Fin du workshop

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import * as EventEmitter from 'events';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
@@ -15,8 +15,25 @@ export class ProductDetailComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private router: Router
   ) { }
+
+  delete() {
+    if(confirm('Are you sure ??')) {
+      this
+        .productService
+        .deleteProduct(this.product.id)
+        .subscribe(
+          () => {
+            console.log('Product deleted from server');
+            this.productService.initProducts();
+            this.router.navigateByUrl('/products');
+          },
+          error => console.log('Failed to delete product with error: ' + error)
+        );
+    }
+  }
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params['id'];
